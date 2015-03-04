2d44a0079976dcb4203297b50edf13b8464886be
Szymon <szymon@sarnowicz.net>
2015-03-03 22:31:27 -0800

    Update minitennis.js

--- minitennis.js
+++ minitennis.js
@@ -1,21 +1,22 @@
 var LeaderBoardCanvas;
 
-function setup() {
-    createCanvas(700, 600); //400, 600
+setup = function() { //p5 setup
+    createCanvas(700, 600); //400, 600: size of game play canvas
     LeaderBoardCanvas = createGraphics(400, 600);
-    
+    //
     frameRate(60);
     noCursor();
-    tennisBall = new Ball(); //ask about .create()
+    //
+    tennisBall = new Ball();
     tennisTable = new Table();
     time = new Time();
     scoreBoard = new LeaderBoard();
 }
 
-function draw() {
+draw = function() { //p5 draw function, runs the code at 60 fps
     background(200, 89, 100); //50 for blue background
     //
-    LeaderBoardCanvas.background(200);          //leaderboard canvas
+    LeaderBoardCanvas.background(200); //leaderboard canvas
     image(LeaderBoardCanvas, 400, 0, 300, 600);
     //
     scoreBoard.display();
@@ -25,9 +26,3 @@ function draw() {
     tennisBall.block.run();
     scoreBoard.display();
 }
-
-var WriteToFile = function()
-{
-    //TODO
-    //look at save();
-};
